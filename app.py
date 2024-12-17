2da42fe087657416f1c0de56599777c77e197d2a
dean <dean.fl20@student.unibi.ac.id>
2024-12-17 08:39:05 +0700

    change to only rename image

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def rename(base_directory):
         print(f"The path {base_directory} is not a valid directory.")
         return
 
+    image_extensions = {'.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp'} 
+
     for root, subdirs, files in os.walk(base_directory):
-        
-        # Skip the base directory itself if there is sub directories
         if not subdirs and root == base_directory:
             continue
 
@@ -29,9 +29,13 @@ def rename(base_directory):
 
         for file_name in files:
             original_path = os.path.join(root, file_name)
-            
+
             # Get file extension
             file_extension = os.path.splitext(file_name)[1]
+
+            # Check if the file is an image
+            if file_extension.lower() not in image_extensions:
+                continue
 
             # Format new name
             new_name = f"{subdir_name}-{counter:07d}{file_extension}"
@@ -47,34 +51,20 @@ def rename(base_directory):
                 tk.Label(top, text="Error renaming file: " + str(e)).pack(pady=20)
                 tk.Button(top, text="OK", command=top.destroy).pack(pady=10)
                 return
-    
+
     top = tk.Toplevel()
     top.geometry("300x120")
     tk.Label(top, text="Files renamed successfully").pack(pady=20)
     tk.Button(top, text="OK", command=top.destroy).pack(pady=10)
 
 window = tk.Tk()
-window.title("Batch Rename")
+window.title("Batch Image Rename")
 window.geometry("400x200")
 
 directory = tk.StringVar()
 directory_entry = tk.Entry(window, textvariable=directory)
 directory_entry.pack(pady=10, padx=50, fill='x')
 tk.Button(window, text="Browse", width=3, command=lambda: browse_directory(directory)).pack(pady=10)
-
-# manual_rename = tk.BooleanVar()
-# tk.Checkbutton(window, text="Manual Rename", variable=manual_rename).pack()
-# new_name = tk.Entry(window)
-# new_name.pack(pady=10, padx=50, fill='x')
-
-# def toggle_entry():
-#     if manual_rename.get():
-#         new_name.config(state='normal')
-#     else:
-#         new_name.config(state='disabled')
-
-# manual_rename.trace_add('write', lambda *args: toggle_entry())
-# toggle_entry() 
 
 tk.Button(window, text="Rename", width=10, command=lambda: rename(directory.get())).pack(pady=10)
 
